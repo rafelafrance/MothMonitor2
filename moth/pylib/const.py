c180ea59e576338197d31dbc3e984a201a7d9b5d
rafelafrance <raphael.lafrance@gmail.com>
2026-01-19 13:50:44 -0500

    Create utility for drawing bounding boxes

--- moth/pylib/const.py
+++
@@ -1,8 +0,0 @@
-# tkinter fonts
-FONT = ("DejaVu Sans", 24)
-FONT_SM = ("DejaVu Sans", 16)
-FONT_SM_I = ("DejaVu Sans", 16, "italic")
-FONT_SM_U = ("DejaVu Sans", 16, "underline")
-FONT_SM_UI = ("DejaVu Sans", 16, "underline italic")
-
-GAP_CHAR: str = "⋄"
